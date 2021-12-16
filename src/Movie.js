6f3403225af2803378a0028decbb194654195375
박다현 (Dahyeon Park) <dahyeon1712@dgu.ac.kr>
2021-12-17 04:52:02 +0900

    Initialize project using Create React App

---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+
+class Movie extends Component {
+
+    render() {
+        return (
+            <div>
+                무비
+            </div>
+        )
+    }
+}
+
+export default Movie
---
+++ src/Tv.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+
+class Tv extends Component {
+
+    render() {
+        return (
+            <div>
+                티비
+            </div>
+        )
+    }
+}
+
+export default Tv
